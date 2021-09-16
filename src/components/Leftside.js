62f20d4696129f1666fc8c8691cfeac3f59c3a4d
GilsonGangadhar <gilsongangadhar@gmail.com>
2021-09-16 16:51:33 +0530

    home page left-side text widget creating

--- src/components/Leftside.js
+++ src/components/Leftside.js
@@ -15,11 +15,37 @@ const Leftside = (props) => {
             </UserInfo>
             <Widget>
                 <a>
+                    <div>
                     <span>Connections</span>
                     <span>Grow your network</span>
+                    </div>
+                    <img src="/images/widget-icon.svg" />
                 </a>
             </Widget>
+            <Item>
+                <span>
+                    <img src="/images/item-icon.svg" />
+                    My items
+                </span>
+            </Item>
         </Artcard>
+
+        <CommunityCard>
+            <a>
+                <span>Groups</span>
+            </a>
+            <a>
+                <span>Events
+                    <img src="/images/plus-icon.svg" />
+                </span>
+            </a>
+            <a>
+                <span>Follow hashtags</span>
+            </a>
+            <a>
+                <span>Discover more</span>
+            </a>
+        </CommunityCard>
     </Container>
 }
 
@@ -85,6 +111,96 @@ line-height : 1.33;
 font-weight : 400; 
 `; 
 
-const Widget = styled.div``; 
+const Widget = styled.div`
+border-bottom : 1px solid rgba(0, 0, 0, 0.15);
+padding-top : 12px; 
+padding-bottom : 12px; 
+
+& > a {
+    text-decoration : none; 
+    display : flex; 
+    justify-content : space-between; 
+    align-items : center; 
+    padding : 4px 12px;
+
+&:hover {
+    background-color : rgba(0, 0, 0, 0.08);
+
+}
+
+div {
+    display : flex;
+    flex-direction : column; 
+    text-align : left; 
+    span {
+        font-size : 12px;
+        line-height : 1.333; 
+        &:first-child{
+            color : rgba(0, 0, 0, 0.4);
+        }
+        &:nth-child(2){
+            color : rgba(0, 0, 0, 1);
+        }
+    }
+}
+}
+
+svg { 
+    color : rgba(0,0,0,1);
+}
+`; 
+
+const Item = styled.a`
+border-color : rgba(0,0,0,0.8);
+text-align : left; 
+padding : 12px; 
+font-size : 12px; 
+display : block;
+span {
+    display : flex; 
+    align-items : center; 
+    color : rgba(0, 0, 0, 1); 
+    svg {
+        color : rgba(0, 0, 0, 0.6)
+    }
+}
+
+&:hover {
+    background-color : rgba(0,0,0,0.08);
+}
+`; 
+
+const CommunityCard = styled(Artcard)`
+padding : 8px 0 0;
+text-align : left; 
+display : flex;
+flex-direction : column; 
+a {
+    color : black;
+    padding : 4px 12px 4px 12px;
+    font-size : 12px; 
+    
+
+&:hover { 
+    color : #0a66c2; 
+}
+span {
+    display : flex;
+    align-items : center;
+    justify-content : space-between;
+}
+
+&:last-child {
+    color : rgba(0,0,0,0.6);
+    text-decoration : none; 
+    border-top : 1px solid #d6cec2;
+    padding : 12px;
+
+    &:hover {
+        background-color : rgba(0,0,0,0.08);
+    }
+}
+}
+`;
 
 export default Leftside
